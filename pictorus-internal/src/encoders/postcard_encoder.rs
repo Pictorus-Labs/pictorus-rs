2d4e8fb1c01d752515ca1f31c0f5ea8bb2a786bf
Austin McElroy <mcelroy.austin@gmail.com>
2025-07-03 10:11:56 -0500

    Changes: - Improved unit test for postcard_encoder, encoding an example of data we pictorus produces.

--- pictorus-internal/src/encoders/postcard_encoder.rs
+++ pictorus-internal/src/encoders/postcard_encoder.rs
@@ -19,22 +19,59 @@ impl PostcardEncoderCOBS {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde::Serialize;
-
-    #[derive(Serialize)]
-    struct LogData {
-        app_time: f64,
-    }
+    use alloc::vec::Vec;
+    use alloc::vec;
+    use serde::{Deserialize, Serialize};
 
     #[test]
-    fn test_postcard_encoder() {
-        let log_data = LogData { app_time: 1.0 };
+    fn test_common_pictorus_log_data() {
+        #[derive(Serialize, Deserialize, Debug)]
+        struct LogData<'a> {
+            pub timestamp: Option<u64>,
+            pub state_name: Option<&'a str>,
+            pub matrix: Option<[[f64; 4]; 4]>,
+            pub byte_array: Option<Vec<u8>>,
+        }
+
+        let matrix = Some([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]);
+        let byte_array = Some(vec![1, 2, 3, 4, 5, 6, 7, 8]);
+        let state_name = Some("test_state");
+        let log_data = LogData {
+            timestamp: Some(1234567890),
+            state_name,
+            matrix,
+            byte_array,
+        };
+
+        // Size the buffer:
+        //   16*8 (16 f64 matrix, no length) + 
+        //   (8 + 1) (byte array + u8 length) + 
+        //   (10 + 1) (state name + u8 length) + 
+        //   (4) (timestamp is a u64, but should be varint encoded to a u32)
+        //   (4) (4 bytes for the 4 options)
+        let mut decode_buffer = [0u8; 160];
 
         let mut encoder = PostcardEncoderCOBS {};
-        let encoded = encoder.encode::<64>(&log_data);
+        let encoded = encoder.encode::<161>(&log_data); // Allocate an extra byte for COBS encoding
 
-        // COBS encoding removes zero bytes and adds a sentinel byte, so the length of an 8 byte float
-        // with some 0's in it will be 10 bytes.
-        assert!(encoded.len() == 10);
+        // Un-COBS-ify
+        let _decode = cobs::decode(&encoded, &mut decode_buffer).expect("Successfully un-COBS-ified");
+
+        let log_data_decoded: LogData = postcard::from_bytes(&decode_buffer).expect("Successfully decoded");
+
+        assert!(log_data_decoded.timestamp == Some(1234567890));
+        assert!(log_data_decoded.state_name == Some("test_state"));
+        assert!(log_data_decoded.matrix == Some([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 16.0],
+        ]));
+        assert!(log_data_decoded.byte_array == Some(vec![1, 2, 3, 4, 5, 6, 7, 8]));
     }
 }
